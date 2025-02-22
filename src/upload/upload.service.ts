179d8c310f306abb90a89f1aaf427b78d74954f3
AlolaSunday12 <alolasj@gmail.com>
2025-02-22 23:32:32 +0100

    update on upload service

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { uploadImages } from './utils/aws';
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  async uploadFiles(files: Array<Express.Multer.File>) {
+    try {
+      return await uploadImages(files);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new InternalServerErrorException(
+          `Image upload service error: ${error.message}`
+);
+      }
+      throw new InternalServerErrorException(
+        'Unknown error in image upload service.'
+ );
+    }
+  }
+}
+
